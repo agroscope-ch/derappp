1342afff3422da39c66da0033956f14f601cb254
Mathis Marcel <marcel.mathis@agroscope.admin.ch>
2026-07-27 11:33:36 +0200

    Add missing mapping for Metaldehyd -> Metaldehyde

--- data_generation/02_substance_maps/srppp.R
+++ data_generation/02_substance_maps/srppp.R
@@ -60,7 +60,8 @@ srppp_map_manual <- tribble(
   "Pyrethrins", "323",
   "Soy lecithin", "988",
   "Sulfur", "338",
-  "Zoxamide", "1404")
+  "Zoxamide", "1404",
+  "Metaldehyde", "Metaldehyd")
 
 # Map for substances that have the same name in the old and the new XML versions
 srppp_map_v1_v2 <- srppp_substances_merged[c("pk_v1", "pk_v2")] |>
