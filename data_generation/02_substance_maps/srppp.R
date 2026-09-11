1295567dba1d09ca8c7595cc951811aa496557fb
Mathis Marcel <marcel.mathis@agroscope.admin.ch>
2026-09-11 17:41:30 +0200

    correction

--- data_generation/02_substance_maps/srppp.R
+++ data_generation/02_substance_maps/srppp.R
@@ -60,8 +60,7 @@ srppp_map_manual <- tribble(
   "Pyrethrins", "323",
   "Soy lecithin", "988",
   "Sulfur", "338",
-  "Zoxamide", "1404",
-  "Metaldehyde", "Metaldehyd")
+  "Zoxamide", "1404")
 
 # Map for substances that have the same name in the old and the new XML versions
 srppp_map_v1_v2 <- srppp_substances_merged[c("pk_v1", "pk_v2")] |>
